d410ebbeb2d96181a783f581c40b55bfab1f8114
alienzj <alienchuj@gmail.com>
2019-03-05 17:28:24 +0800

    support recursive download

--- gitdir.py
+++ gitdir.py
@@ -17,7 +17,10 @@ def create_url(url):
 	modifying the given url so that it returns JSON data when
 	we do a GET requests later in this script
 	"""
-	
+
+	# extract the dirs name from the given url (e.g master)
+	download_dirs = re.findall(r"\/tree\/master\/(.*)", url)[0]
+
 	# extract the branch name from the given url (e.g master)
 	branch = re.findall(r"\/tree\/(.*?)\/", url)[0]
 	
@@ -26,7 +29,65 @@ def create_url(url):
 
 	api_url = api_url+"?ref="+branch
 
-	return api_url
+	return (api_url, download_dirs)
+
+def download(repo_url):
+	'''
+	recursive download
+	'''
+
+	# generate the url which returns the JSON data
+	api_url, download_dirs = create_url(repo_url)
+
+	r = urllib.request.urlretrieve(api_url)
+    
+	# make a directory with the name which is taken from
+	# the actual repo
+	os.makedirs(download_dirs, exist_ok=True)
+
+	# total files count
+	total_files = 0
+
+	with open(r[0], "r") as f:
+		raw_data = f.read()
+        
+		data = json.loads(raw_data)
+
+		# getting the total number of files so that we
+		# can use it for the output information later
+		total_files += len(data)
+
+		for index, file in enumerate(data):
+			file_url = file["download_url"]
+			fname = file["name"]
+			path = file["path"]
+
+			if not file_url is None:
+				try:
+					# download the file
+					urllib.request.urlretrieve(file_url, path)
+
+					# bring the cursor to the beginning, erase the current line, and dont make a new line
+					print("\r"+ERASE_LINE, end="")
+					print("\033[1;92m▶ Downloaded {}/{}: \033[0m{}".format(index+1, total_files, fname), end="", flush=True)
+
+				except KeyboardInterrupt:
+		    		# when CTRL+C is pressed during the execution of this script,
+					# bring the cursor to the beginning, erase the current line, and dont make a new line
+					print("\r"+ERASE_LINE, end="")
+
+					# we are not doing index+1 in this line because when the user does CTRL+C, the file next file wont
+					# wont be downloaded and this line will show that it has downloaded 1 extra file than it really
+					# has.
+					#
+					# lets say this script downloaded 4 files, the line below, would show that it had dowloaded
+					# 5 files IF there was index+1
+					print("\033[1;92m✔ Got interupted, but was able to download {} of {} files: \033[0m".format(index, total_files))
+					exit()
+			else:
+				download(file["html_url"])
+
+	return total_files
 
 
 def main():
@@ -40,52 +101,7 @@ def main():
 
 	repo_url = args.url
 
-	download_dir = repo_url.split("/")[-1]
-
-	# generate the url which returns the JSON data
-	api_url = create_url(repo_url)
-
-	r = urllib.request.urlretrieve(api_url)
-    
-	with open(r[0], "r") as f:
-		raw_data = f.read()
-        
-	data = json.loads(raw_data)
-
-	# make a directory with the name which is taken from the 
-	# actual repo
-	os.makedirs(download_dir, exist_ok=True)
-
-	# getting the total number of files so that we
-	# can use it for the output information later
-	total_files = len(data)
-
-	for index, file in enumerate(data):
-		file_url = file["download_url"]
-		fname = file["name"]
-		path = file["path"]
-		
-		try:
-			# download the file
-			urllib.request.urlretrieve(file_url, path)
-
-			# bring the cursor to the beginning, erase the current line, and dont make a new line
-			print("\r"+ERASE_LINE, end="")
-			print("\033[1;92m▶ Downloaded {}/{}: \033[0m{}".format(index+1, total_files, fname), end="", flush=True)
-
-		except KeyboardInterrupt:
-		    # when CTRL+C is pressed during the execution of this script,
-			# bring the cursor to the beginning, erase the current line, and dont make a new line
-			print("\r"+ERASE_LINE, end="")
-
-			# we are not doing index+1 in this line because when the user does CTRL+C, the file next file wont
-			# wont be downloaded and this line will show that it has downloaded 1 extra file than it really
-			# has.
-			#
-			# lets say this script downloaded 4 files, the line below, would show that it had dowloaded
-			# 5 files IF there was index+1
-			print("\033[1;92m✔ Got interupted, but was able to download {} of {} files: \033[0m".format(index, total_files))
-			exit()
+	total_files = download(repo_url)
 
 	print("\r"+ERASE_LINE, end="")
 	print("\033[1;92m✔ Finished downloading {} file(s)\033[0m".format(total_files))
