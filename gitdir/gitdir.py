333c95149ddb3a5fe6eef167007bc39baa516e43
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-20 13:33:06 -0800

    Can specify multiple URLS, output dir, and download files.
    I added support for the following features: specifying multiple files and directories, via the command line.
    Specifying an output directory. 
    Git paths containing blob.
    Downloading files via download.

--- gitdir/gitdir.py
+++ gitdir/gitdir.py
@@ -32,35 +32,43 @@ def print_text(text, color="default", in_place=False, **kwargs):  # type: (str, 
 
 def create_url(url):
     """
-    modifying the given url so that it returns JSON data when
-    we do a GET requests later in this script
+    From the given url, produce a URL that is compatible with Github's REST API. Can handle blob or tree paths.
     """
 
     # extract the branch name from the given url (e.g master)
-    branch = re.findall(r"/tree/(.*?)/", url)[0]
-
-    # extract the dirs name from the given url (e.g master)
-    download_dirs = re.findall(r"/tree/" + branch + r"/(.*)", url)[0]
-
+    branch = re.findall(r"/tree/(.*?)/", url)
     api_url = url.replace("https://github.com", "https://api.github.com/repos")
-    api_url = re.sub(r"/tree/.*?/", "/contents/", api_url)
+    if len(branch) == 0:
+        branch = re.findall(r"/blob/(.*?)/", url)[0]
+        download_dirs = re.findall(r"/blob/" + branch + r"/(.*)", url)[0]
+        api_url = re.sub(r"/blob/.*?/", "/contents/", api_url)
+    else:
+        branch = branch[0]
+        download_dirs = re.findall(r"/tree/" + branch + r"/(.*)", url)[0]
+        api_url = re.sub(r"/tree/.*?/", "/contents/", api_url)
 
     api_url = api_url + "?ref=" + branch
+    return api_url, download_dirs
 
-    return (api_url, download_dirs)
 
-
-def download(repo_url, flatten):
-    '''
-    recursive download
-    '''
+def download(repo_url, flatten=False, output_dir="./"):
+    """ Downloads the files and directories in repo_url. If flatten is specified, the contents of any and all
+     sub-directories will be pulled upwards into the root folder. """
 
     # generate the url which returns the JSON data
     api_url, download_dirs = create_url(repo_url)
 
-    try:
-        r = urllib.request.urlretrieve(api_url)
+    # To handle file names.
+    if not flatten:
+        if len(download_dirs.split(".")) == 0:
+            dir_out = os.path.join(output_dir, download_dirs)
+        else:
+            dir_out = os.path.join(output_dir, "/".join(download_dirs.split("/")[0:-1]))
+    else:
+        dir_out = output_dir
 
+    try:
+        response = urllib.request.urlretrieve(api_url)
     except KeyboardInterrupt:
         # when CTRL+C is pressed during the execution of this script,
         # bring the cursor to the beginning, erase the current line, and dont make a new line
@@ -70,45 +78,60 @@ def download(repo_url, flatten):
     if not flatten:
         # make a directory with the name which is taken from
         # the actual repo
-        os.makedirs(download_dirs, exist_ok=True)
+        os.makedirs(dir_out, exist_ok=True)
 
     # total files count
     total_files = 0
 
-    with open(r[0], "r") as f:
+    with open(response[0], "r") as f:
         raw_data = f.read()
 
         data = json.loads(raw_data)
-
         # getting the total number of files so that we
         # can use it for the output information later
         total_files += len(data)
 
+        # If the data is a file, download it as one.
+        if data["type"] == "file":
+            try:
+                # download the file
+                urllib.request.urlretrieve(data["download_url"], os.path.join(dir_out, data["name"]))
+                # bring the cursor to the beginning, erase the current line, and dont make a new line
+                print_text("Downloaded: " + Fore.WHITE + "{}".format(data["name"]), "green", in_place=True)
+
+                return total_files
+            except KeyboardInterrupt:
+                # when CTRL+C is pressed during the execution of this script,
+                # bring the cursor to the beginning, erase the current line, and dont make a new line
+                print_text("✘ Got interrupted", 'red', in_place=False)
+                exit()
+
         for index, file in enumerate(data):
             file_url = file["download_url"]
-            fname = file["name"]
+            file_name = file["name"]
 
             if flatten:
                 path = os.path.basename(file["path"])
             else:
                 path = file["path"]
 
-            if not file_url is None:
+            path = os.path.join(dir_out, path)
+            if file_url is not None:
                 try:
                     # download the file
                     urllib.request.urlretrieve(file_url, path)
 
                     # bring the cursor to the beginning, erase the current line, and dont make a new line
-                    print_text("Downloaded: " + Fore.WHITE + "{}".format(fname), "green", in_place=True, end="",
+                    print_text("Downloaded: " + Fore.WHITE + "{}".format(file_name), "green", in_place=False, end="",
                                flush=True)
 
                 except KeyboardInterrupt:
                     # when CTRL+C is pressed during the execution of this script,
                     # bring the cursor to the beginning, erase the current line, and dont make a new line
-                    print_text("✘ Got interrupted", 'red', in_place=True)
+                    print_text("✘ Got interrupted", 'red', in_place=False)
                     exit()
             else:
-                download(file["html_url"], flatten)
+                download(file["html_url"], flatten, dir_out)
 
     return total_files
 
@@ -119,18 +142,20 @@ def main():
         signal.signal(signal.SIGTSTP, signal.SIG_IGN)
 
     parser = argparse.ArgumentParser(description="Download directories/folders from GitHub")
-    parser.add_argument('url', action="store")
+    parser.add_argument('urls', nargs="+",
+                        help="List of Github directories to download.")
+    parser.add_argument('--output_dir', "-d", dest="output_dir", default="./",
+                        help="All directories will be downloaded to the specified directory.")
 
-    parser.add_argument('--flatten', '-f', action="store_true", help='Flatten directory structures. Do not create extra'
-                                                                     ' directory and download found files to current'
-                                                                     ' directory.')
+    parser.add_argument('--flatten', '-f', action="store_true",
+                        help='Flatten directory structures. Do not create extra directory and download found files to'
+                             ' current directory.')
 
     args = parser.parse_args()
 
-    repo_url = args.url
     flatten = args.flatten
-
-    total_files = download(repo_url, flatten)
+    for url in args.urls:
+        total_files = download(url, flatten, args.output_dir)
 
     print_text("✔ Download complete", "green", in_place=True)
 
