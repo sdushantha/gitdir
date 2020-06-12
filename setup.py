a4efa56983739a800e7f49a0ba21711680c43bf3
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-06-12 08:44:39 +0200

    version bump 1.2.3 --> 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='gitdir',
-    version='1.2.3',
+    version='1.2.4',
     author='Siddharth Dushantha',
     author_email='siddharth.dushantha@gmail.com',
     description='Download a single directory/folder from a GitHub repo',
